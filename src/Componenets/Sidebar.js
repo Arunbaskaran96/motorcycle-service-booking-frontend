3100d88f28041c51b82c4a219b2d2fe9162c978d
Arunbaskaran96 <arundhi04101996@gmail.com>
2023-02-28 18:19:32 +0530

    vehicle-service-booking-frontend

--- src/Componenets/Sidebar.js
+++ src/Componenets/Sidebar.js
@@ -8,7 +8,6 @@ function Sidebar() {
     <div className='side-conatiner'>
         <ul className='side-lists'>
           <Link className='sidebar-li' to='/portal/home' >Service</Link><br></br>
-          <Link className='sidebar-li' to='/portal/shop' >Shops</Link><br></br>
           <Link className='sidebar-li' to='/portal/userhistory' >History</Link><br></br>
         </ul>
     </div>
